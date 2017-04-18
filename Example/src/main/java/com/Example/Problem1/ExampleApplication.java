8e44333eba15d184092b695ae23e356b19fdf3ee
Rushikesh Pathak <rdppathak@gmail.com>
2017-04-18 15:03:16 +0530

    Initialized sample Java Spring project using STS

---
+++ Example/src/main/java/com/Example/Problem1/ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.Example.Problem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+}
