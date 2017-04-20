bb52d40519e79b73076b404bfbdbc5ae0562481a
Rushikesh Pathak <rdppathak@gmail.com>
2017-04-20 10:37:53 +0530

    Added new project for TestNg practice session

---
+++ TestNgPractice/src/main/java/com/Testing/TestNgSample/TestNgPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.Testing.TestNgSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNgPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNgPracticeApplication.class, args);
+	}
+}
